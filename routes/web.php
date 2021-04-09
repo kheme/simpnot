c3192fb0650066a1ba0021f4627184ea006cb257
Okiemute Omut <iamkheme@gmail.com>
2021-04-09 15:55:41 +0100

    Create endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+/** @var \Laravel\Lumen\Routing\Router $router */
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->post('subscribe/{topic}',      'SubscriptionController@store');
+$router->post('publish/{topic}',        'BroadcastController@store');
+$router->delete('subscribe/{topic}',    'SubscriptionController@destroy');
+
+$router->post('{topic}', function (Request $request, string $topic) {
+    Log::info($request->all());
+    
+    return 'Broadcast received';
 });
